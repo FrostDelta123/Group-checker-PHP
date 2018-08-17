829093b75aa7b02496f0e58c311a9de768a3f78d
FrostDelta123 <pawel.soko2012@yandex.ru>
2018-08-17 12:52:06 +0300

    Install commit
    Install commit

---
+++ dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", ""); 
+
+define ("DBNAME", "");
+
+define ("DBUSER", "");
+
+define ("DBPASS", "");  
+
+define ("PREFIX", "dle"); 
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8"); 
+
+define('SECURE_AUTH_KEY', '');
+
+$db = new db;
+ 
+?>
